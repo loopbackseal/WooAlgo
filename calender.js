e5ed642dd2c5209cee196994031fda8e99800b93
loopbackseal <young961027@gmail.com>
2021-10-25 17:05:25 +0900

    calender

---
+++ calender.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    let answer = '';
+    const month = [31,29,31,30,31,30,31,31,30,31,30,31];
+    const day = ["THU","FRI","SAT","SUN","MON","TUE","WED",];
+    let i = 0;
+    let days = b;
+    while (i < a - 1) {
+        days = days + month[i];
+        i++;
+    }
+    answer = day[days % 7];
+    return answer;
+}
